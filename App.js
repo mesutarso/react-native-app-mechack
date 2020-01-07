9a255b9eadc0943a8eaada191b1c90f97cd7623a
mechack Tambwe <mechacktambwe@yahoo.com>
2020-01-07 16:28:08 +0100

    app.js modified

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+export default class App extends React.Component{
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>My first app</Text>
+      </View>
+    );
+  }
+  }
+ 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'green',
+    backgroundColor: '#f0f3f5',
     alignItems: 'center',
     justifyContent: 'center',
   },
