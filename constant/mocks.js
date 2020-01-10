a408dd7cd7e2e93127eff81f2a4c5f824f81bb28
mechack Tambwe <mechacktambwe@yahoo.com>
2020-01-10 14:44:53 +0100

    first app for blood

---
+++ constant/mocks.js
@@ -0,0 +1,69 @@
+const requests = [
+    {
+      id: 1,
+      bloodType: "B+",
+      name: "Merveille Kitoko",
+      age: 24,
+      gender: "homme",
+      hopital: "hopital general de Kinshasa",
+      time: 12,
+      priority: "urgent",
+    },
+    {
+      id: 2,
+      bloodType: "O-",
+      name: "Mechack Tambwe",
+      age: 19,
+      gender: "homme",
+      hopital: "Cliniques Ngaliema",
+      time: 22,
+      priority: "urgent",
+    },
+    {
+      id: 3,
+      bloodType: "A+",
+      name: "Junior Ngangeli",
+      age: 25,
+      gender: "homme",
+      hopital: "hopital general de Kinshasa",
+      time: 24,
+      priority: "urgent",
+    },
+    {
+        id: 4,
+        bloodType: "A+",
+        name: "joel Ambu",
+        age: 26,
+        gender: "homme",
+        hopital: "hopital general de Kinshasa",
+        time: 24,
+        priority: "urgent",
+      },
+  
+  ];
+  
+  const chart = [
+    1.1,
+    3,
+    1.5,
+    2.3,
+    3.2,
+    7,
+    8.2,
+    1.2,
+    2,
+    1.2,
+    8,
+    3.8,
+    5.8,
+    3.9,
+    5.1,
+    0.1,
+    6
+  ];
+  
+  const user = {
+    avatar: require('../assets/avatar.png')
+  };
+  
+  export { requests, chart, user }
